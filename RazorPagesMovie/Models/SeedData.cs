ea0ea427f7a668c2aa9457271743326693c22540
boomiarul10 <boomiarul10@gmail.com>
2022-09-22 17:26:38 +0530

    Frst Commit

---
+++ RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+
+namespace RazorPagesMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                if (context == null || context.Movie == null)
+                {
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                }
+
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "beast",
+                        ReleaseDate = DateTime.Parse("2022-2-5"),
+                        Genre = "Action",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Chandramugi",
+                        ReleaseDate = DateTime.Parse("1995-3-13"),
+                        Genre = "Comedy,thriller",
+                        Price = 8.99M,
+                        Rating = "G"
+                    },
+
+                    new Movie
+                    {
+                        Title = "zombie",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Thriller",
+                        Price = 9.99M,
+                        Rating = "R"
+                    },
+
+                    new Movie
+                    {
+                        Title = "angri Bird",
+                        ReleaseDate = DateTime.Parse("2019-4-15"),
+                        Genre = "cartoon",
+                        Price = 3.99M,
+                        Rating = "R"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
+
